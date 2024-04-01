6f7bfc75080384f3fec8d2130a980192ee86553d
Roman <brysommer1@gmail.com>
2024-04-01 17:39:39 +0300

    wfp

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,32 +1,39 @@
 import { NextResponse } from "next/server";
-
+import * as crypto from 'crypto';
 import { wfp } from "@/lib/wayforpay";
 import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
     
   const body = await req.text();
-  console.log(body);
+  
+  const object = JSON.parse(body);
 
-  let data;
-
-  data = wfp.parseAndVerifyIncomingWebhook(JSON.parse(body));
-  /*
-  try {
-    data = wfp.parseAndVerifyIncomingWebhook(JSON.parse(body));
-  } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
-  }
-  */  
-  console.log(`DATA: ${data}`);
-  console.log(data?.products[0]);
+  let data = object
+  //validation response
+  const forHash = [
+    data.merchantAccount,
+    data.orderReference,
+    data.amount,
+    data.currency,
+    data.authCode,
+    data.cardPan,
+    data.transactionStatus,
+    data.reasonCode,
+    ].join(';');
+    const expectedMerchantSignature = crypto
+        .createHmac('md5', process.env.WAY_FOR_PAY_SECRET!)
+        .update(forHash)
+        .digest('hex');
+    if (expectedMerchantSignature !== data.merchantSignature) {
+        throw new Error('Corrupted webhook received. Webhook signature is not authentic.');
+    }
 
   const metadata = data?.products[0].name.split(',');
   const userId = metadata[1];
   const courseId = metadata[0];
 
   if (data.transactionStatus === 'Approved') {
-    console.log(data);
     
     if (!userId || !courseId) {
       return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
@@ -43,7 +50,18 @@ export async function POST(req: Request) {
   } else {
     return new NextResponse(`Webhook Error: Unhandled event type`, { status: 200 })
   }
-  const answer = wfp.prepareSignedWebhookResponse(data);
+
+
+  const answer = {
+    orderReference: data.orderReference,
+    status: 'accept',
+    time: Date.now(),
+    signature: '',
+  };
+  const forHashString = [answer.orderReference, answer.status, answer.time].join(';');
+  const hash = crypto.createHmac('md5', process.env.WAY_FOR_PAY_SECRET!).update(forHashString).digest('hex');
+  answer.signature = hash;
+  
   new NextResponse(JSON.stringify(answer), { status: 200 });
   return new NextResponse(null, { status: 200 });
 }
