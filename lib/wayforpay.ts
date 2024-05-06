427b6433e9d43e76f7caa530320eaed7ca49b5c3
Roman <brysommer1@gmail.com>
2024-05-06 13:02:29 +0300

    wfp

--- lib/wayforpay.ts
+++ lib/wayforpay.ts
@@ -3,7 +3,7 @@ import { WFP, WFP_CONFIG } from 'overshom-wayforpay';
 WFP_CONFIG.DEFAULT_PAYMENT_CURRENCY = 'UAH';
 
 export const wfp = new WFP({
-    MERCHANT_ACCOUNT: 'itgin_online',
+    MERCHANT_ACCOUNT: 'academylog_com_ua',
     MERCHANT_SECRET_KEY: process.env.WAY_FOR_PAY_SECRET!,
     MERCHANT_DOMAIN_NAME: process.env.NEXT_PUBLIC_APP_URL!,
     // service URL needed to receive webhooks
